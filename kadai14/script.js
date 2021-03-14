d89e52238bcf50b59497aa65319175bf6facf1a4
katsuhiko-mozya <kitamurahiko@gmail.com>
2021-03-14 18:16:14 +0900

    a

---
+++ kadai14/script.js
@@ -0,0 +1,22 @@
+function showitem(){
+    var number = document.getElementById("number1").value
+    switch(number){
+        case  "100":
+            window.alert("ok");
+            break;
+        case  "5":
+            window.alert("ok");
+            break;
+        case  "777":
+            window.alert("ok");
+            break;
+        case  "8888":
+            window.alert("ok");
+        break;
+       default:
+       break;
+    };
+    console.log(number)
+};
+
+
