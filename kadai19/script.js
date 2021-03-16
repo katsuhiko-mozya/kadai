53f4e5fe8f0f611fbb761e5ceeb639d975632985
katsuhiko-mozya <kitamurahiko@gmail.com>
2021-03-16 18:30:04 +0900

    a

--- kadai19/script.js
+++ kadai19/script.js
@@ -1,10 +1,12 @@
 function showitem(){
-    const array =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    array.forEach(function(index)  {
-        console.log(index)
-        for (let  = 0; index < array.length; index++) {
-            
-        }
-        window.alert(a)
-        });
+    let array =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let x=0
+    array.forEach(i => {
+        y=x+i
+        x=y
+    
+          
+        
+    });
+        window.alert(y)
 };
