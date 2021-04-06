47681a6053acd8063877ac51fc7bfea9476ee840
katsuhiko-mozya <kitamurahiko@gmail.com>
2021-04-06 15:10:07 +0900

    a

--- kadai23/script2.js
+++ kadai23/script2.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
         jsonpCallback: 'jsondata',
             success: function(json){
                 json.forEach(function(data,index){
+                    console.log(data)
                     let newrow = "<tr> <th>顧客ID</th><td>" + data.顧客ID + "</td></tr><tr><th>顧客名</th><td>" + data.顧客名 + "</td></tr><tr><th>住所</th><td>" + data.住所 + "</td></tr><tr><th>電話番号</th><td>" + data.電話番号 + "</td></tr><tr><th>年齢</th><td>" + data.年齢 +"</td></tr>";
                     $("tbody").append(newrow);  
                                         });
