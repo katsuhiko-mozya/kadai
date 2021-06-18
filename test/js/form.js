3c69b59703c048e2caa3edb085aa92e1b6616ba8
katsuhiko-mozya <kitamurahiko@gmail.com>
2021-06-18 19:08:49 +0900

    a

--- test/js/form.js
+++ test/js/form.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
     var param = decodeURI(location.search).split("&")
-    
+    console.log(param)
    
    $.each(param,function(index,value) {
-    //let p = "<span>" + value.replace("?","").replace("name=","").replace("tel=","").replace("mail=","").replace("toiawase=","") + "</span>";
-    //$("#info2").append(p); 
-   let newrow =  value.replace("?","").replace("name=","").replace("tel=","").replace("mail=","").replace("toiawase=","") ;
+    let output = value.replace("?","").split("=") // ["name","あ"]
+    console.log(output[1]) // = あ
+
+    // $("tr:eq(" + String(index) + ")>td:eq(1)").text(output[1])
+
+    switch(index){
+      case 0: 
+      $("span.name").text(output[1])
+          break;
+        case 1: $("span.tel").text(output[1])
+          break;
+        case 2: $("span.mail").text(output[1])
+          break;
+        case 3: $("span.memo").text(output[1])
+          break;
+          default:
+            break;
+    }
    
-   
- $("tbody>tr>td:eq(1)").append(value);
+
+ //$("tbody>tr>td:eq(1)").append(value);
  }); 
 });
