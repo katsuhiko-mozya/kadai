747a21fc290862f1a587a508358510e24d7daca7
katsuhiko-mozya <kitamurahiko@gmail.com>
2021-07-08 15:51:06 +0900

    a

--- 追加課題/add2.js
+++ 追加課題/add2.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
     const param= location.search
     console.log(param)
     //urlにパラメータをつける
-    url2="https://ateliercloudllc.bitrix24.jp/rest/1/pzxzq747zf5dgg89/crm.product.list"+param;
+    //.list で全体取得
+    //.get　指定したIDのみ取得                                                      ↓
+    url2="https://ateliercloudllc.bitrix24.jp/rest/1/pzxzq747zf5dgg89/crm.product.get"+param;
     //ajaxでdata読み込み
 console.log(param,url2)
 $.ajax({
@@ -14,8 +16,10 @@ $.ajax({
 })
 .done(function (response2) {
     console.log(response2)
-   
-                    
+    item=response2.result
+    let row = "<tr><td>"+ item.TIMESTAMP_X +"</td><td>"+ item.DESCRIPTION +"</td><td>" + item.CURRENCY_ID +"</td></tr>"
+    console.log(row)
+    $("tbody").append(row);                
     
 })
 })
