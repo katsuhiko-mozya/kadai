505c57c1c5e574a6c3d1650a626da355cfb51d75
katsuhiko-mozya <kitamurahiko@gmail.com>
2021-07-08 15:34:10 +0900

    a

--- 追加課題/add2.js
+++ 追加課題/add2.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
     //パラメータ取得
     const param= location.search
+    console.log(param)
     //urlにパラメータをつける
-    url="https://ateliercloudllc.bitrix24.jp/rest/1/pzxzq747zf5dgg89/crm.product.list"+param;
+    url2="https://ateliercloudllc.bitrix24.jp/rest/1/pzxzq747zf5dgg89/crm.product.list"+param;
     //ajaxでdata読み込み
-console.log(param,url)
+console.log(param,url2)
 $.ajax({
     type: "GET",
-    url:　url,
+    url:　url2,
     dataType: 'json',
    timeout:　3000,
 })
