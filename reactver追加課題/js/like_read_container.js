1558a49dda7bd79c19bb3f7ea0227c8a9df0308b
katsuhiko-mozya <kitamurahiko@gmail.com>
2021-07-20 17:35:23 +0900

    a

--- reactver追加課題/js/like_read_container.js
+++ reactver追加課題/js/like_read_container.js
@@ -8,58 +8,50 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Likeform = function (_React$Component) {
-  _inherits(Likeform, _React$Component);
+var Likeread = function (_React$Component) {
+  _inherits(Likeread, _React$Component);
 
-  function Likeform(props) {
-    _classCallCheck(this, Likeform);
+  function Likeread(props) {
+    _classCallCheck(this, Likeread);
 
-    var _this = _possibleConstructorReturn(this, (Likeform.__proto__ || Object.getPrototypeOf(Likeform)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (Likeread.__proto__ || Object.getPrototypeOf(Likeread)).call(this, props));
 
-    _this.state = { NAME: "", PRICE: "" };
-    _this.valuechange = _this.valuechange.bind(_this);
-    _this.valuesubmit = _this.valuesubmit.bind(_this);
+    _this.state = { liked: false };
     return _this;
   }
 
-  _createClass(Likeform, [{
-    key: "valuechange",
-    value: function valuechange(event) {
-      var target = event.target;
-      var name1 = target.name;
-    }
-  }, {
-    key: "valuesubmit",
-    value: function valuesubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
-  }, {
-    key: "render",
+  _createClass(Likeread, [{
+    key: 'render',
     value: function render() {
       return React.createElement(
-        "form",
-        { onSubmit: this.valueSubmit },
+        'table',
+        null,
         React.createElement(
-          "label",
+          'thead',
           null,
-          "Name:",
-          React.createElement("input", { id: "input2", type: "text", value: this.state.NAME, onChange: this.valuechange }),
-          React.createElement("br", null)
+          React.createElement(
+            'th',
+            null,
+            'TIMESTAMP_X'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'DESCRIPTION'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'CURRENCY_ID'
+          )
         ),
-        React.createElement(
-          "label",
-          null,
-          "PRCE:",
-          React.createElement("input", { id: "input3", type: "number", value: this.state.PRICE, onChange: this.valuechange })
-        ),
-        React.createElement("input", { type: "submit", value: "Submit" })
+        React.createElement('tbody', null)
       );
     }
   }]);
 
-  return Likeform;
+  return Likeread;
 }(React.Component);
 
-var domContainer = document.querySelector('#like_cform_container');
-ReactDOM.render(React.createElement(Likeform, null), domContainer);
+var domContainer = document.querySelector('#like_read_container');
+ReactDOM.render(React.createElement(Likeread, null), domContainer);
--- reactver追加課題/src/js/like_cform_container.js
+++ reactver追加課題/src/js/like_cform_container.js
@@ -1,30 +1,44 @@
 'use strict';
 
 class Likeform extends React.Component {
-  constructor(props) {//
-    super(props);
-    this.state = {value: ''};//valueというstateを宣言
-
-    this.handleChange = this.handleChange.bind(this);//handleChangeを宣言
-    this.handleSubmit = this.handleSubmit.bind(this);//handlesubmitを宣言
+  constructor(props){
+    super(props)
+    this.state={NAME:"",PRICE:""};
+    this.valuechange=this.valuechange.bind(this);
+    this.valuesubmit=this.valuesubmit.bind(this);
   }
 
-  handleChange(event) {this.setState({value: event.target.value});
-  }//valueを変更するイベント
+ valuechange(event) {
+  switch (event.target.name) {
+    case "NAME":
+      this.setState({NAME: event.target.value});
+      break;
+      case "PRICE":
+        this.setState({PRICE: event.target.value});
+       break;
+  
+    default:
+      break;
+  }
+  };
 
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }//A name was submitted: ' + this.state.valueと表示するイベント
+ valuesubmit(event){
+   alert('A name was submitted: ' + this.state.NAME+this.state.PRICE);
+ event.preventDefault();
+ }
 
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.valueSubmit}>
         <label>
           Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
+          <input name="NAME" type="text" value={this.state.NAME} onChange={this.valuechange} /><br/>
+          </label>
+          <label>
+            PRCE:
+            <input name="PRICE" type="number" value={this.state.PRICE} onChange={this.valuechange} />
+          </label>
         <input type="submit" value="Submit" />
       </form>
     );
