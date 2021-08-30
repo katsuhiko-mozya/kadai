a3733267ac07bc1553f18270dc7188c3dad4a1a3
katsuhiko-mozya <kitamurahiko@gmail.com>
2021-08-30 21:49:02 +0900

    a

--- python3/create.py
+++ python3/create.py
@@ -1,43 +1,45 @@
 import json
 import module
-# 'python3/info_cus.json'読み込み
-info_open = open('python3/info_cus.json', 'r')
+
+info_load=[]
+
 try:
-    info_load =json.load(info_open)
+        # 'python3/info_cus.json'読み込み
+    with open('python3/info_cus.json', 'r') as info_open:
+        info_load=json.load(info_open)
 except:
     info_load=[]  
+   
+
 
 # 入力
 flag=0
-id=int(input("個人IDを入力してください:"))
+id=input("個人IDを入力してください:")
 while True:
     if  not module.unique(id,info_load):
         id=input("個人IDを再入力してください:")
     else:
         break
+
 name=input("名前を入力してください:")
 tel=input("電話番号を入力してください")
 ad_num=input("郵便番号を入力してください:")
 ad=input("住所を入力してください:")
 main=input("お問い合わせ内容を入力してください:")
 
-for x in info_load:
-    if name == x["name"] and tel == x["tel"]:
-        x["ad_num"]=ad_num
-        x["ad"]=ad
-        x["main"]=main
-        f = open('python3/info_cus.json', 'w')  
-        json.dump(x, f, ensure_ascii=False) 
-        f.close
-        flag=1
-        break
-
+if  not len(info_load)==0: #配列の和検証
+    for x in info_load:
+        if name == x["name"] and tel == x["tel"]:
+            x["ad_num"]=ad_num
+            x["ad"]=ad
+            x["main"]=main
+            flag=1
+    
 if flag==0:   
     info = {'id':id,'name':name,'tel':tel,'ad_num':ad_num,'ad':ad,'main':main}
     info_load.append(info)
-    # 'python3/info_cus.json'に書き込み
-    f = open('python3/info_cus.json', 'w')  
-    json.dump(info_load, f, ensure_ascii=False) 
+
+with open('python3/info_cus.json', 'w')  as f:
+    # json.dumps(info_load)
+    f.write(json.dumps(info_load))
     f.close
-
-
---
+++ python3/search.py
@@ -0,0 +1,28 @@
+# ◆情報の検索を行うプログラムの要件
+# 氏名、電話番号、顧客IDで検索できるようにする
+# 検索は、完全一致とする（入力内容と登録内容が完全に一致する場合のみ結果が返る）
+# 情報があった場合、すべての情報を改行を入れて出力する
+# 情報が見つからなかった場合は見つからなかったことを表示する
+# jsonファイルへのアクセスに失敗する想定もこと
+import json
+try:
+    info_open = open('python3/info_cus.json', 'r')
+    info_load =json.load(info_open)
+except:
+    print("データが読み取れませんでした")
+
+print("顧客情報を検索します")
+id=input("個人IDを入力してください:")
+name=input("名前を入力してください:")
+tel=input("電話番号を入力してください")
+
+flag=0
+for s in info_load:
+    if id==s["id"] and name==s["name"] and tel==s["tel"]:
+        for p in s.values():
+            print(p)
+            flag=1
+    else:
+        flag=0
+if flag==0:
+    print("データが見つかりません")
