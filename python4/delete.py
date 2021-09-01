146ea13e8700cc47beff8dfd4a793ca27136efae
katsuhiko-mozya <kitamurahiko@gmail.com>
2021-09-02 00:52:04 +0900

    あ

--- python4/delete.py
+++ python4/delete.py
@@ -29,9 +29,7 @@ while True:
 # 配列順番特定
 count=0
 count=module.count(count,info_load,id)
-# print(info_load[count]["name"])
-# print(info_load[count]["main"].replace(",","")[1])
-print(info_load[count]["main"].replace(",","")[1])
+
 #全体削除           
 if all_part =="all":
     for i in info_load[count].values():
@@ -51,88 +49,59 @@ elif all_part=="part":
             print(index,i)
     select=input("何番の情報を削除しますか?1,2,3,4,5:")
     while True:
-        if select=="1" or select=="" or select=="3" or select=="4" or select=="5"  :
+        if select=="1" or select=="2" or select=="3" or select=="4" or select=="5"  :
             break
         else:
             select=input("入力が間違っています。もう一度入力してください。何番の情報を削除しますか?(1,2,3,4,5:")
+    number=""
     if select=="1":
-        print(info_load[count]["name"])
-        judge=input("以上の情報を削除します。本当によろしいですか？(yes/no)")
-        if module.judge(judge):
-            try:
-                info_load[count]["name"]=""
-            except:
-                print("削除に失敗しました。もう一度やり直してください。")    
-        else:
-            print("削除を中止しました。")
-
+        number="name"
     elif select=="3":
-        print(info_load[count]["ad_num"])
-        judge=input("以上の情報を削除します。本当によろしいですか？(yes/no)")
-        if module.judge(judge):
-            try:
-                info_load[count]["ad_num"]=""
-            except:
-                print("削除に失敗しました。もう一度やり直してください。")    
-        else:
-            print("削除を中止しました。") 
-
+        number="ad_num"
     elif select=="4":
-        print(info_load[count]["ad"])
-        judge=input("以上の情報を削除します。本当によろしいですか？(yes/no)")
-        if module.judge(judge):
-            try:
-                info_load[count]["ad"]=""
-            except:
-                print("削除に失敗しました。もう一度やり直してください。")    
-        else:
-            print("削除を中止しました。")
-
+        number="ad"
     elif select=="2":
-        try:
-            for index,i in enumerate(info_load[count]["tel"].replace(",",""),1):
-                print(index,i)
-        except:
-            print("情報が存在しません")
-        del_part=input("どの番号の情報を削除しますか？番号で答えてください(ex.1,2,3...)：")
-        try:
-            print(info_load[count]["tel"].replace(",","")[del_part-1])
-        except:
-            print("番号が一致しません。もう一度最初からやり直してください。")
+        number="tel"
+    elif select=="5":
+        number="main"
+
+    if select=="1"  or select=="3" or select=="4":
+        print(info_load[count][number])
         judge=input("以上の情報を削除します。本当によろしいですか？(yes/no)")
         if module.judge(judge):
             try:
-                info_load[count]["tel"].replace(",","")[del_part-1]=""
+                info_load[count][number]=""
             except:
                 print("削除に失敗しました。もう一度やり直してください。")    
         else:
             print("削除を中止しました。")
-
-    elif select=="5":
+    else:
         while True:
             try:
-                for index,i in enumerate(info_load[count]["main"].replace(",",""),1):
+                for index,i in enumerate(info_load[count][number].replace(",",""),1):
                     print(index,i)
             except:
                 print("情報が存在しません")
                 break
             del_part=int(input("どの番号の情報を削除しますか？番号で答えてください(ex.1,2,3...)："))-1
             try:
-                print(info_load[count]["main"].replace(",","")[del_part])
+                print(info_load[count][number].replace(",","")[del_part])
             except:
                 print("番号が一致しません。もう一度最初からやり直してください。")
                 break
             judge=input("以上の情報を削除します。本当によろしいですか？(yes/no)")
             if module.judge(judge):
+                # 質問
                 # try:
-                    info_load[count]["main"].replace((info_load[count]["main"].replace(",","")[del_part-1]),"")
+                    info_load[count][number].replace((info_load[count][number].replace(",","")[del_part-1]),"")
                     break
                 # except:
                 #     print("削除に失敗しました。もう一度やり直してください。")    
                 #     break
             else:
                 print("削除を中止しました。")
-                break
+                break        
+
 # 書込み
 with open('python4/info_cus.json', 'w')  as f:
     # json.dump(info_load, f, ensure_ascii=False) 
--- python4/module.py
+++ python4/module.py
@@ -13,15 +13,6 @@ def judge(judge):
         else:
             judge=input("yesかnoで答えてください")
 
-def display(x,id):
-    if id=="idnone":
-        for i,p in enumerate(x.values(),0):
-            if not i==0:
-                print(i,p)            
-    else:
-        for i,p in enumerate(x.values(),1):
-            print(i,p)
-
 def count(count,info_load,id):
     for id_array in info_load:
         if  id == id_array["id"]:
